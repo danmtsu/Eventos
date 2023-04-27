695a808bf3299dae482a68f1604033789e876c92
danmtsu <daniel.martinstsuji@gmail.com>
2023-04-27 16:47:54 -0300

    acabando o sistema, com a ultima página por enquanto. Novidades em breve hehehe

--- cliente/views.py
+++ cliente/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from eventos.models import Certificado
+from eventos.models import Certificado, Evento
 # Create your views here.
 def meus_certificados(request):
     certificados = Certificado.objects.filter(participante=request.user)
     return render(request, 'meus_certificados.html', {'certificados':certificados})
+
+
+def meus_eventos(request):
+    if request.method == "GET":
+        eventos = Evento.objects.filter(participantes=request.user)
+        return render(request, 'meus_eventos.html' , {'eventos':eventos})
+    
